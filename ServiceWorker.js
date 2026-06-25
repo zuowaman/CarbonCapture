6e1a1927d075f4bfbb7885f59795a45312846c20
zuowaman <evan9003@gmail.com>
2026-06-25 13:27:46 +0800

    更新音效

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-CarbonCapture-1.0";
+const cacheName = "DefaultCompany-CarbonCapture-2.0";
 const contentToCache = [
     "Build/WebGlLV1.loader.js",
     "Build/WebGlLV1.framework.js",
